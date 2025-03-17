b555e68b46c0ab17e61bab8a6dd39b348f5cf23c
YadBro <ncada708@gmail.com>
2025-03-17 17:53:09 +0700

    remove gameconfig require

--- src/shared/init.lua
+++ src/shared/init.lua
@@ -1,7 +1,6 @@
 return {
     utils = {
         run = require(script:WaitForChild("utils").run),
-        GameConfig = require(script:WaitForChild("utils").GameConfig.MainModule),
         Timer = require(script:WaitForChild("utils").Timer.MainModule),
     }
 }
